36810adbf6e733e363f9859e7978ffb06a6c2ea1
zhenya <mikhalyov.2808@mail.ru>
2022-12-10 16:24:37 +0300

    add solution JavaSyntax task 0103

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0103/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task01.task0103;
+
+/* 
+Сбросить Диего и сверхсекретный груз
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Сбрасываем Диего");
+        System.out.println("Сбрасываем сверхсекретный груз");
+    }
+}
