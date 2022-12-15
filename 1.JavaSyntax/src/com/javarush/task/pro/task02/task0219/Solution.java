747c84dc09bd4d9d066eae0c812be15984add4ed
zhenya <mikhalyov.2808@mail.ru>
2022-12-15 17:09:55 +0300

    add solution JavaSyntax task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 6;
+        int age = 5;
+        String documents = "documents";
+        String behavior = "behavior";
+    }
+}
