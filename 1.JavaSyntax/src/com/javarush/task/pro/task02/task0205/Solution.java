c685ace87735341e6f1004ef34e6c0ac0e61998e
zhenya <mikhalyov.2808@mail.ru>
2022-12-24 16:27:16 +0300

    add solution JavaSyntax task 0205

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0205/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0205/Solution.java
@@ -8,8 +8,16 @@ public class Solution {
 
     public static void main(String[] args) {
         int toothCounter = 32;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
 
-        //напишите тут ваш код
 
 
         System.out.println(toothCounter);
