fcb13a417fdba84b6b75bf63b6e6fd74b1f9e0a0
zhenya <mikhalyov.2808@mail.ru>
2022-12-24 16:27:27 +0300

    add solution JavaSyntax task 0216

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -6,7 +6,13 @@ package com.javarush.task.pro.task02.task0216;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        String name = "Alice";
+        String date = "3.10.22";
+        String time = "14:22";
+        String noun = "school";
+        String adjective = "wonderfull";
+        String verb = "go";
+
 
         System.out.println("Новости:");
         System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
