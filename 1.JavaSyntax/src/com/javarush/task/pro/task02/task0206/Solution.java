651f7bab4c5ee1e790e310af3700ba0ad810219d
zhenya <mikhalyov.2808@mail.ru>
2022-12-24 16:49:59 +0300

    add solution JavaSyntax task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "word", phrase = "PHRASE", line = "line", text = "text";
+
+
+    }
+}
