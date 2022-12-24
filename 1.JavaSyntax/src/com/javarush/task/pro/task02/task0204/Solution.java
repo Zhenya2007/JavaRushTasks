c662df0147d999031208e1696158bc4768874109
zhenya <mikhalyov.2808@mail.ru>
2022-12-24 16:27:05 +0300

    add solution JavaSyntax task 0204

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0204/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0204/Solution.java
@@ -8,8 +8,11 @@ public class Solution {
 
     public static void main(String[] args) {
         int six = 6;
+        six++;
+        six++;
+        six++;
 
-        //напишите тут ваш код
+
 
 
         System.out.println(six);
