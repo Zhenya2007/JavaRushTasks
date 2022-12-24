9c9dca9e93c1acd2ffe31774fa584542cb22ad1d
zhenya <mikhalyov.2808@mail.ru>
2022-12-24 16:26:34 +0300

    add solution JavaSyntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Apple.java
@@ -0,0 +1,4 @@
+package com.javarush.task.pro.task01.task0101;
+
+public class Apple {
+}
--- 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
@@ -7,6 +7,6 @@ package com.javarush.task.pro.task01.task0101;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println("Диего + Амиго = Друзья");
+        System.out.println("Скоро новый ГОД!!!!!!!!!!!!!!!");
     }
 }
