7f60c7afd601411e6aa10b806c974b26519f55f9
zhenya <mikhalyov.2808@mail.ru>
2023-02-12 11:11:50 +0300

    add solution JavaSyntax task 0408

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -8,7 +8,14 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        while (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            if (max < number && number % 2 == 0) {
+                max = number;
+            }
+        }
+        System.out.println(max);
     }
 }
