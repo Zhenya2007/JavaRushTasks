d6b9470ff53a5d161c2b3a38607b0be99adb3ae5
zhenya <mikhalyov.2808@mail.ru>
2023-02-21 12:58:57 +0300

    add solution JavaSyntax task 0425 part 3/4

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -15,10 +15,12 @@ public class Solution {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (i == 0 && j == 3) {
+                if (i == 8 && j == 3) {
                     System.out.print(robot);
                 } else if (i == 9) {
                     System.out.print(bug);
+                } else if (j == 3) {
+                    System.out.print(hole);
                 } else {
                     System.out.print(land);
                 }
