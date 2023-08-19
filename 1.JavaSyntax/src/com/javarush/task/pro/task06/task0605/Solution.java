2958d6eabcd2d35abfc692d635ee4f43272a8de1
zhenya <mikhalyov.2808@mail.ru>
2023-08-19 13:11:03 +0300

    add solution JavaSyntax task 0605

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -14,7 +14,27 @@ public class Solution {
     }
 
     public static void reverseArray(int[] array) {
+//        int temp0 = array[0];
+//        array[0] = array[array.length - 1];
+//        array[array.length - 1] = temp0;
+//
+//        int temp1 = array[1];
+//        array[1] = array[array.length - 2];
+//        array[array.length - 2] = temp1;
+//
+//        int temp2 = array[2];
+//        array[2] = array[array.length - 3];
+//        array[array.length - 3] = temp2;
+//
+//        int temp3 = array[3];
+//        array[3] = array[array.length - 4];
+//        array[array.length - 4] = temp3;
 
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
     }
 
     public static void printArray(int[] array) {
