52b256a19bb670d66774762425fbc3304ee6c3b8
zhenya <mikhalyov.2808@mail.ru>
2023-09-24 12:41:42 +0300

    add solution JavaSyntax task 0616 - 0718

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -5,5 +5,10 @@ package com.javarush.task.pro.task07.task0703;
 */
 
 public class Solution {
-    //напишите тут ваш код
+    byte aByte = Byte.MIN_VALUE;
+    short aShort = Short.MIN_VALUE;
+    int anInt = Integer.MIN_VALUE;
+    long aLong = Long.MIN_VALUE;
+    char aChar = Character.MIN_VALUE;
+
 }
