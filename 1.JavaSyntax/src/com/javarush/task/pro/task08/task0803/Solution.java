34dd419cc1c8b5451eeabb10fef6fe19711c342b
zhenya <mikhalyov.2808@mail.ru>
2023-11-05 13:03:11 +0300

    add solution JavaSyntax task 0616 - 0805

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0803/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task08.task0803;
+
+import java.util.Scanner;
+
+/* 
+Минимальный элемент массива
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] intArray = getArrayOfTenElements();
+        System.out.println(min(intArray));
+    }
+
+    public static int min(int[] ints) {
+        int min = ints[0];
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] < min) {
+                min = ints[i];
+
+            }
+        }
+        return min;
+
+    }
+
+    public static int[] getArrayOfTenElements() {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            int number = scanner.nextInt();
+            array[i] = number;
+        }
+        return array;
+
+
+    }
+}
